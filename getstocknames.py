431ba07010bc205f9a423655750d4c3a9acb36f8
Aparna <hkaparna@gmail.com>
2018-07-10 04:30:32 +0530

    First  folder

---
+++ getstocknames.py
@@ -0,0 +1,8 @@
+stock=["","","","",""]
+for i in range(0,4):            #To enter the names of stock items                                 
+    stock[i]=raw_input("Enter the names of stock")  
+print "\n \tSTOCK";
+print "......................."
+print "\n"
+for i in range(0,4):
+    print "\t",stock[i]                                 
